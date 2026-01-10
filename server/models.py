368e4942e9b2edf5cd431fd85e5aa8ff2792f173
Bonarerijt <prudyjj4@gmail.com>
2026-01-11 00:34:11 +0300

    Created the skeleton for my models.py file

---
+++ server/models.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+
+class Episode(db.Model, SerializerMixin):
+    pass
+
+
+class Appearance(db.Model, SerializerMixin):
+    pass
+
+
+class Guest(db.Model, SerializerMixin):
+    pass
